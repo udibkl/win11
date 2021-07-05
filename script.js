503c15dd461f42d6840db58374763cf41e368d39
udibkl <68611381+udibkl@users.noreply.github.com>
2021-07-05 21:46:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let taskbar = document.getElementsByClassName("taskbar")[0]
 let startmenu = document.getElementsByClassName("startmenu")[0]
-
+alert("Please Press F11");
 taskbar.addEventListener("click", ()=>{
     console.log("clicked");
     if(startmenu.style.bottom == "710px"){
